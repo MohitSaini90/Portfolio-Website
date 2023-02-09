ad8a7135f6db0cfe7a6b0b059f267012113399eb
Mohit <mohitsainidna@gmail.com>
2023-02-09 22:06:04 +0530

    This commit is to deploy Flask in Production mode and not in by default production mode

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
-
+from waitress import serve
 app = Flask(__name__)
 app.static_folder = 'static'
 @app.route('/')
 def index():
     return render_template('index.html')
-app.run(debug=True)
+# Use mode='dev' for Flask in development mode and mode='prod' for flask in production mode
+mode="prod" 
+if __name__ == "__main__":
+    if mode=="dev":
+        app.run(host='0.0.0.0' , port=50100, debug=True)
+    else:
+        serve(app, host='127.0.0.1', port=5000, threads=1)
+
